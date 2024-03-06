607efeb69c0dd61523cdd7530957d13941cd8122
Trupti <patiltruptib@gmail.com>
2024-03-06 14:12:06 -0800

    Added Header Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,30 @@
 import "./Header.scss";
+import { Link } from "react-router-dom";
+import logoImage from "./../../assets/logo/cart-logo.png";
+import CartImage from "./../../assets/images/cart.png";
 
 function Header() {
-  return <h1>Header</h1>;
+  return <header className="header">
+
+    <div className="header__column">
+      <Link to="/">
+        <img className="header__logo" alt="ProductLogo" src={logoImage} />
+      </Link>
+    </div>
+
+    <div className="header__title header__column"><h1>ClickCart</h1></div>
+
+    <div className="header__column">
+      <Link className="header__link" to="/">
+        <div>HOME</div>
+      </Link>
+      <div className="header__cart">
+        <Link className="header__link" to="/cart">
+          <img className="header__cart-image" alt="CartImage" src={CartImage} />
+        </Link>
+      </div>
+    </div>
+
+  </header>
 }
 export default Header;
